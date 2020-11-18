f4976d40e5025e256e179e5170362e9594480871
Junaid <Junaid.562000@gmail.com>
2020-11-18 13:33:17 +0530

    search and display meals from api

---
+++ script.js
@@ -0,0 +1,53 @@
+const search = document.getElementById("search"),
+  submit = document.getElementById("submit"),
+  random = document.getElementById("random"),
+  mealsEl = document.getElementById("meals"),
+  resultHeading = document.getElementById("result-heading"),
+  single_mealEl = document.getElementById("single-meal");
+
+// Search meal and fetch from API
+
+function searchMeal(e) {
+  e.preventDefault();
+
+  //   clear single meal
+  single_mealEl.innerHTML = "";
+
+  //   Get search term
+  const term = search.value;
+
+  //   Check for empty
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        resultHeading.innerHTML = `<h2>Search result for ${term} :</h2>`;
+        if (data.meals == null) {
+          resultHeading.innerHTML =
+            "<p>There is no search result . Try again !</p>";
+        } else {
+          meals.innerHTML = data.meals
+            .map(
+              (meal) => `
+            <div class="meal">
+            <img src="${meal.strMealThumb}" alt ="${meal.strMeal}"/>
+            <div class="meal-info" data-mealID="${meal.idMeal}">
+             <h3>${meal.strMeal}</h3>
+            </div>
+            </div>
+            `
+            )
+            .join("");
+        }
+      });
+
+    //   clear search text
+
+    search.value = "";
+  } else {
+    alert("Please enter a search term");
+  }
+}
+
+//   Event Listeners
+submit.addEventListener("submit", searchMeal);
